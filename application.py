dbf722cf639d432c57323bde3137ef2014031f4d
LeonitaNika <58749225+LeonitaNika@users.noreply.github.com>
2021-02-13 21:21:25 +0100

    Krijimi i dritares kryesore dhe funksioneve

--- application.py
+++ application.py
@@ -1,49 +1,55 @@
+#importimi i librarive
 import os
 from tkinter import *
-from tkinter import messagebox, filedialog
+from tkinter import messagebox
+from tkinter import filedialog
+from guizero import  error
+import tkinter as tk
 import json
 import requests
-from pathlib import Path
+from PIL import Image, ImageTk
 
-
-def upload():
-    
-    Button(window, text='File', bg="#F0F8FF", command=uploadd).place(relx=0.5, rely=0.5, anchor=CENTER)
-
-def uploadd():
-
-    file = filedialog.askopenfilename(title="Open file")
-    name = Path(file).stem
-    extension = os.path.splitext(file)[1]
-    if extension != ".doc" and extension != ".docx" and extension != ".xls" and extension != ".xlsx":
-        print("Dokumenti duhet te  kete ekstension .doc,.doxc,.xls ose .xlsx")
-        messagebox.showinfo("Error", "Nuk lejohet ruajtja e dokumentit te tille")
-        messagebox.showinfo("Informacion", "Dokumenti duhet te  kete ekstension .doc,.doxc,.xls ose .xlsx")
-    else:
-        headers = {
-        "Authorization": "Bearer "
-                         "ya29.A0AfH6SMA3xNK3SsiMBh-GELHA5OzNswxynJUCPD9jK1dj5V92a2ONWF6jSuD1cJXJRLckA_bvxFE1tW-_ik3O1W-P785e9ypZnIbhTBypKC6rgCoJxudiwrdVex5PPeKBvB0DH5coWI1qlO_hlYrcm2tGSF-I"}
-        para = {
-        "name": name,
-        "parents": ["1KUB53Qiow3i1Rs_ufhAZJX-o07G1hFYs"]
-         }
-
-        files = {
-        'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),
-        'file': open(file, "rb")
-        }
-        r = requests.post(
-            "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
-            headers=headers,
-            files=files
-
-        )
-        print(r.text)
-    
+#krijimi i nje dritareje permes tkinter
 window = Tk()
-window.title("Ngarkimi i fajllave ne Google Drive")
-window.geometry('450x150')
+
+#ndryshimi i specifikave te dritares
 window.configure(bg='#1569C7')
+window.title("Ngarkimi i fajllit ne Google Drive")
+window.geometry('500x200')
+#vendosja e ikones te aplikacionit
+window.iconbitmap("img/up.ico")
+
+#krijimi i  variablave te tipit string
+token=token_var = tk.StringVar()
+folder_var = tk.StringVar()
+
+#krijimi i label l dhe vendosja e specifikave te saj
+l = Label(window, text="  Ngarkimi i fajllave ne Google Drive permes Python ",font=('calibre', 12, 'normal'),fg='white',bg='#1569C7')
+
+#percaktimi i pozicionit te label-it ne dritare
+l.place(relx=0.1,rely=0.3,anchor=NW)
+
+#vendosja e fotos me size te percaktuar
+image = Image. open('img/fotoo.jpg')
+image = image. resize((60, 50), Image. ANTIALIAS)
+my_img = ImageTk. PhotoImage(image)
+panel = Label(window, image = my_img)
+
+#percaktimi i pozicionit te fotos ne dritare
+panel.place(relx=0.4,rely=0.5,anchor=NW)
+
+
+#krijimi i funksionit upload
+def upload():
+    #fshirja e labels te caktuar pas shtypjes se menus Upload
+    panel.destroy()
+    l.destroy()
+
+    #krijimi i labels dhe percaktimi i specifikate te tyre
+    tk.Label(window, text='Token', font=('calibre', 11, 'normal'), fg='white',bg='#1569C7').place(relx=0.1, rely=0.1, anchor=NE)
+    tk.Label(window, text='Folder', font=('calibre', 11, 'normal'), fg='white',bg='#1569C7').place(relx=0.1, rely=0.25, anchor=NE)
+
+
 
 
 def helpp():
